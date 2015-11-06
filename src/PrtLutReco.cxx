3dc3e1d5450202b92455015c5125758d9690baa6
Roman Dzhygadlo <r.dzhygadlo@gsi.de>
2015-11-06 02:47:32 +0100

    set limit for p0

--- src/PrtLutReco.cxx
+++ src/PrtLutReco.cxx
@@ -329,9 +329,9 @@ Bool_t PrtLutReco::FindPeak(Double_t& cherenkovreco, Double_t& spr, Double_t a){
     fFit->SetParameters(100,cherenkovreco,0.010,10);   // peak
     // fFit->SetParameter(1,cherenkovreco);   // peak
     // fFit->SetParameter(2,0.005); // width
-    fFit->SetParameter(0,1000); 
+    fFit->SetParLimits(0,0,1E12); 
     fFit->SetParLimits(2,0.008,0.020); // width
-    fHist->Fit("fgaus","M","",cherenkovreco-0.15,cherenkovreco+0.15);
+    fHist->Fit("fgaus","M","",cherenkovreco-0.1,cherenkovreco+0.1);
     fHist->Fit("fgaus","M","",cherenkovreco-0.04,cherenkovreco+0.04);
     cherenkovreco = fFit->GetParameter(1);
     spr = fFit->GetParameter(2); 
