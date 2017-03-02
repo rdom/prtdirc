3f26a897d808754fb98edf93c6e9063b972739cf
Roman Dzhygadlo <r.dzhygadlo@gsi.de>
2017-03-02 12:02:11 +0100

    fix default

--- src/PrtPixelSD.cxx
+++ src/PrtPixelSD.cxx
@@ -218,7 +218,7 @@ G4bool PrtPixelSD::ProcessHits(G4Step* step, G4TouchableHistory* hist){
   // step->GetPreStepPoint()->GetGlobalTime()*1000
 
   Bool_t quantum_efficiency(true);
-  Bool_t charge_sharing(false);
+  Bool_t charge_sharing(true);
   Bool_t dead_time(true);
   Bool_t dark_counts(true);
   Bool_t transport_efficiency(true);
